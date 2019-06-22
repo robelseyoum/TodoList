6178a153624f0859ebf425482d9e7adfffac44de
Robel <robelseyoum3@gmail.com>
2019-06-22 18:03:10 +0100

    expressjs for todolists first

---
+++ README.md
@@ -0,0 +1,22 @@
+# TodoLists with ExpressJS
+
+
+This project uses
+- Uses a MongoDB
+- MongoDB Driver
+- EJS Template Engine
+- Form Processing
+- CRUD Functionality
+
+
+In order to run the app, install the dependencies first with...
+- npm install
+- npm install -g bower
+ - bower install bootstrap
+ - bower install jquery
+
+Then run the app with
+- npm start
+
+OR if you have nodemon installed (npm install -g nodemon)
+- nodemon
---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+//port
+const port = 3000;
+
+//int app
+const app = express();
+
+//mongo dababase location identification
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const url = "mongodb://localhost:27017/todolist";
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//View Setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Connect to mongodb
+MongoClient.connect(url, (err, database) => {
+  console.log("MongoDB Connected...");
+  if (err) throw err;
+
+  db = database;
+  Todos = db.collection("todos");
+
+  //used not to keep connecting the server for every db connection
+  app.listen(port, () => {
+    console.log("Server running on port " + port);
+  });
+});
+
+//routers
+app.get("/", (req, res, next) => {
+  //fetch data from mongodb table 'todos'
+  Todos.find({}).toArray((err, todos) => {
+    if (err) {
+      return console.log(err);
+    }
+    //console.log(todos);
+    //pass to the views
+    res.render("index", {
+      todos: todos
+    });
+  });
+});
+
+//post request route
+app.post("/todo/add", (req, res, next) => {
+  //create to do object to add to the mongodb
+  const todo = {
+    text: req.body.text,
+    body: req.body.body
+  };
+
+  //Insert the todo object into db
+  Todos.insert(todo, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Todo added..");
+    res.redirect("/");
+  });
+});
+
+//delete request route
+app.delete("/todo/delete/:id", (req, res, next) => {
+  const query = { _id: ObjectID(req.params.id) };
+  Todos.deleteOne(query, (err, response) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Todo Removed");
+    res.send(200);
+  });
+});
